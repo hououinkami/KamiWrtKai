ab7399e49fec2f627131d39c9e046e41df158aa9
hououinkami <hououinkami@gmail.com>
2020-05-26 20:57:57 +0800

    Update diy-before-feed.sh

--- diy-before-feed.sh
+++ diy-before-feed.sh
@@ -1,28 +1,25 @@
-cd package
 #
 # 自定义插件
 #
 # 编译OpenClash
-git clone -b master https://github.com/vernesong/OpenClash.git
+git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 # 添加Clash内核
-mkdir -p base-files/files/etc/openclash/core
-cd base-files/files/etc/openclash/core
+mkdir -p package/base-files/files/etc/openclash/core
+cd package/base-files/files/etc/openclash/core
 wget https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 chmod 0755 clash
 cd -
 # 编译OpenAppFilter
-git clone https://github.com/destan19/OpenAppFilter.git && mv -f OpenAppFilter/* ./
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # 编译ServerChan
-git clone https://github.com/tty228/luci-app-serverchan.git
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #
 # 自定义主题
 #
 # 更新argon主题
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git
-cd lean
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial
+cd package/lean
 rm -rf luci-theme-argon
 cd -
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-#
-cd ./
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
