eb2724d900aed283e158de450912cc92e17a984c
hououinkami <hououinkami@gmail.com>
2020-09-27 08:40:56 +0800

    修复错误

--- diy-after-feed.sh
+++ diy-after-feed.sh
@@ -5,7 +5,7 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' pa
 # sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
 
 # 更改默认语言
-sed -i 's/zh_cn/ja/g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/zh_cn/ja/g' package/lean/default-settings/files/zzz-default-settings
 # 新增日语语言文件
 # cp -r ../../LanguageFile/package .
 # cp -r ../../LanguageFile/feeds .
