93a92e8ee231c27befcd5156870e7afb30b06c8a
hououinkami <hououinkami@gmail.com>
2021-03-08 15:26:39 +0800

    测试

--- diy-before-feed.sh
+++ diy-before-feed.sh
@@ -3,14 +3,23 @@ cd package
 #
 # 自定义插件
 #
-# 编译OpenClash
+# > 编译OpenClash
 git clone -b master https://github.com/vernesong/OpenClash.git
-# 添加Clash内核
+# >> 添加Clash dev内核
 mkdir -p base-files/files/etc/openclash/core
 cd base-files/files/etc/openclash/core
 wget https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz
-tar -zxvf clash-linux-amd64.tar.gz
+tar -zxvf clash-linux-amd64.tar.gz && rm -rf clash-linux-amd64.tar.gz
 chmod 0755 clash
+# >> 添加Clash game内核
+wget https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-amd64.tar.gz
+tar -zxvf clash-linux-amd64.tar.gz -O > clash_game && rm -rf clash-linux-amd64.tar.gz
+chmod 0755 clash_game
+# >> 添加Clash tun内核
+find ../../../../../OpenClash/core-lateset/premium -maxdepth 0  -name "clash-linux-amd64*"  | xargs -i mv -f {} ./
+gzip -df *.gz
+mv clash-linux-amd64* clash_tun
+chmod 0755 clash_tun
 cd -
 # 编译OpenAppFilter
 git clone https://github.com/destan19/OpenAppFilter.git && mv -f OpenAppFilter/* ./
