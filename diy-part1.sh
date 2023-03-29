b557c3e32dd188c0f6491274bca5f47e492ad812
hououinkami <hououinkami@gmail.com>
2023-03-30 00:00:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -162,11 +162,11 @@ cd -
 git clone https://github.com/hououinkami/KamiWrtBot.git
 
 # HelloWorld
-# rm -rf helloworld
-# git clone --depth=1 https://github.com/fw876/helloworld.git
-# sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/*.lua
-# sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/*.htm
+rm -rf helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git
+sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/*.lua
+sed -i 's/services/vpn/g' helloworld/luci-app-ssr-plus/luasrc/view/shadowsocksr/*.htm
 
 # Speedtest
 # git clone https://github.com/sirpdboy/netspeedtest.git
